6b1b2bdd25bed258b70a0e7d09c6f3c189eb73c4
hyun <caudeutsch@gmail.com>
2023-07-17 22:10:46 +0900

    프로젝션 연습

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class JpaMain {
@@ -44,11 +45,47 @@ public class JpaMain {
 
             }*/
 
-            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
+    /*        Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
                     .setParameter("username", "member1")
                     .getSingleResult();
-            System.out.println("singleResult : " + singleResult);
+            System.out.println("singleResult : " + singleResult);*/
 
+            em.flush();
+            em.clear();
+
+            //프로젝션
+       /*     List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+            //result 결과값 모두 영속성에서 관리됨.
+            System.out.println("findMember age : " + findMember.getAge());*/
+
+            // 둘다 같은 조인문이 나가지만 첫번째(묵시적 조인)는 예측하기 힘들기 떄문에 후자 방식으로 사용 권장
+            //List<Team> team = em.createQuery("select m.team from Member m", Team.class)
+        //    List<Team> team = em.createQuery("select t from Member m join m.team t", Team.class)
+        //            .getResultList();
+
+            //임베디드 타입 프로젝션 - 소속되어 있는 엔티티에서 꺼내와야함 from Order는 가능 / from Address는 불가능
+        //    em.createQuery("select o.address from Order o", Address.class)
+        //            .getResultList();
+
+            //스칼라 타입 프로젝션
+            //객체 값을 받아야하는 경우 Object[]로 받는 방법 - 비추
+/*            List<Object[]> resultList = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+
+            Object[] results = resultList.get(0);
+            System.out.println("username = " + results[0]);
+            System.out.println("age = " + results[1]);
+*/
+            // new 명령어로 조회 - 많이 사용
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println("memberDTO username = " + memberDTO.getUsername());
+            System.out.println("memberDTO age = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e){
